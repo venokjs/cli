1a7d9e30455dd0ded6bee5a9c56795a4cfd77996
0x303133 <0x303133@proton.me>
2023-08-02 22:59:44 +0300

    feat: Create Abstract Action Class

---
+++ actions/abstract.action.ts
@@ -0,0 +1,9 @@
+import { Input } from "../commands/abstract.command";
+
+export abstract class AbstractAction {
+  public abstract handle(
+    inputs?: Input[],
+    options?: Input[],
+    extraFlags?: string[],
+  ): Promise<void>;
+}
