bc0201079eb3ab80cddccc329a0e02adc41d832f
0x303133 <0x303133@proton.me>
2023-08-02 23:09:00 +0300

    feat: Create Abstract Command Class

---
+++ commands/abstract.command.ts
@@ -0,0 +1,15 @@
+import {Command} from "commander";
+
+import {AbstractAction} from "../actions/abstract.action";
+
+export interface Input {
+  name: string;
+  value: boolean | string;
+  options?: any;
+}
+
+export abstract class AbstractCommand {
+  protected constructor(protected action: AbstractAction) {}
+
+  public abstract load(program: Command): void;
+}
