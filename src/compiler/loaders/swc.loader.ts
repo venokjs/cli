ed25a90fe2b23ce87f08b89ed7d993e09d2505b1
0x303133 <0x303133@proton.me>
2023-08-03 18:49:42 +0300

    feat: Create swc loader

---
+++ src/compiler/loaders/swc.loader.ts
@@ -0,0 +1,45 @@
+import { Options } from "@swc/core";
+
+import { ERROR_PREFIX } from "../../ui";
+
+interface CliOptions {
+  readonly outDir: string;
+  readonly outFile: string;
+  /**
+   * Invoke swc using transformSync. It's useful for debugging.
+   */
+  readonly sync: boolean;
+  readonly sourceMapTarget?: string;
+  readonly filename: string;
+  readonly filenames: string[];
+  readonly extensions: string[];
+  readonly watch: boolean;
+  readonly copyFiles: boolean;
+  readonly includeDotfiles: boolean;
+  readonly deleteDirOnStart: boolean;
+  readonly quiet: boolean;
+}
+
+type SwcBinary = {
+  default: ({ cliOptions, swcOptions }: { cliOptions: CliOptions; swcOptions: Options }) => void;
+};
+
+export class SwcBinaryLoader {
+  private swcBinary?: SwcBinary;
+
+  public load(): SwcBinary {
+    if (this.swcBinary) return this.swcBinary;
+
+    try {
+      const swcBinary = require("@swc/cli/lib/swc/dir");
+      this.swcBinary = swcBinary;
+      return swcBinary;
+    } catch (err) {
+      console.error(
+        ERROR_PREFIX +
+          ' Failed to load "@swc/cli" and "@swc/core" packages. Please, install them by running "npm i -D @swc/cli @swc/core".',
+      );
+      process.exit(1);
+    }
+  }
+}
