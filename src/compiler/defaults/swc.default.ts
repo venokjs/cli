0c2a1c84d0c239f9f3ddc63c7158a8d6d30a1c9d
0x303133 <0x303133@proton.me>
2023-08-03 20:01:07 +0300

    feat: Add swc defaults

---
+++ src/compiler/defaults/swc.default.ts
@@ -0,0 +1,56 @@
+import ts from "typescript";
+
+import { Configuration } from "../../configuration";
+
+export const swcDefaultsFactory = (tsOptions?: ts.CompilerOptions, configuration?: Configuration) => {
+  const builderOptions =
+    typeof configuration?.compilerOptions?.builder !== "string" ? configuration?.compilerOptions?.builder?.options : {};
+
+  return {
+    swcOptions: {
+      module: {
+        type: "commonjs",
+      },
+      jsc: {
+        target: "es2021",
+        parser: {
+          syntax: "typescript",
+          decorators: true,
+          dynamicImport: true,
+        },
+        transform: {
+          legacyDecorator: true,
+          decoratorMetadata: true,
+          useDefineForClassFields: false,
+        },
+        keepClassNames: true,
+        baseUrl: tsOptions?.baseUrl,
+        paths: tsOptions?.paths,
+      },
+      minify: false,
+      swcrc: true,
+    },
+    cliOptions: {
+      outDir: tsOptions?.outDir ? convertPath(tsOptions.outDir) : "dist",
+      filenames: [configuration?.sourceRoot ?? "src"],
+      sync: false,
+      extensions: [".js", ".ts"],
+      copyFiles: false,
+      includeDotfiles: false,
+      quiet: false,
+      watch: false,
+      ...builderOptions,
+    },
+  };
+};
+
+/**
+ * Converts Windows specific file paths to posix
+ * @param windowsPath
+ */
+function convertPath(windowsPath: string) {
+  return windowsPath
+    .replace(/^\\\\\?\\/, "")
+    .replace(/\\/g, "/")
+    .replace(/\/\/+/g, "/");
+}
