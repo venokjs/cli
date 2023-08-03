55afd335e49fbb3439d6a4503e0626c871c935d1
0x303133 <0x303133@proton.me>
2023-08-03 18:15:27 +0300

    feat: Add CLI errors

---
+++ src/ui/errors.ts
@@ -0,0 +1,6 @@
+export const CLI_ERRORS = {
+  MISSING_TYPESCRIPT: (path: string) =>
+    `Could not find TypeScript configuration file "${path}". Please, ensure that you are running this command in the appropriate directory (inside Nest workspace).`,
+  WRONG_PLUGIN: (name: string) =>
+    `The "${name}" plugin is not compatible with Venok CLI. Neither "after()" nor "before()" nor "afterDeclarations()" function have been provided.`,
+};
