1f0970a28f160692acd2b6ef0c1b460ccd453e7c
0x303133 <0x303133@proton.me>
2023-08-03 18:29:25 +0300

    feat: Create Base compiler

---
+++ src/compiler/base.compiler.ts
@@ -0,0 +1,33 @@
+import { dirname, join, normalize, relative } from "path";
+
+import { getValueOrDefault } from "../helpers/getters/value.or.default";
+import { PluginsLoader } from "./loaders/plugin.loader";
+import { Configuration } from "../configuration";
+
+export abstract class BaseCompiler<T = Record<string, any>> {
+  constructor(private readonly pluginsLoader: PluginsLoader) {}
+
+  public abstract run(
+    configuration: Required<Configuration>,
+    tsConfigPath: string,
+    appName: string | undefined,
+    extras?: T,
+    onSuccess?: () => void,
+  ): any;
+
+  public loadPlugins(configuration: Required<Configuration>, tsConfigPath: string, appName: string | undefined) {
+    const pluginsConfig = getValueOrDefault(configuration, "compilerOptions.plugins", appName);
+    const pathToSource = this.getPathToSource(configuration, tsConfigPath, appName);
+
+    return this.pluginsLoader.load(pluginsConfig, { pathToSource });
+  }
+
+  public getPathToSource(configuration: Required<Configuration>, tsConfigPath: string, appName: string | undefined) {
+    const sourceRoot = getValueOrDefault<string>(configuration, "sourceRoot", appName, "sourceRoot");
+    const cwd = process.cwd();
+    const relativeRootPath = dirname(relative(cwd, tsConfigPath));
+    return normalize(sourceRoot).indexOf(normalize(relativeRootPath)) >= 0
+      ? join(cwd, sourceRoot)
+      : join(cwd, relativeRootPath, sourceRoot);
+  }
+}
