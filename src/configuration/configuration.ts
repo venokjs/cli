a8887e70a160c9c3be2cc3d0b369f3626571c541
0x303133 <0x303133@proton.me>
2023-08-03 17:53:58 +0300

    feat: Add configuration

---
+++ src/configuration/configuration.ts
@@ -0,0 +1,90 @@
+export type Asset = "string" | AssetEntry;
+
+export interface AssetEntry {
+  glob: string;
+  include?: string;
+  flat?: boolean;
+  exclude?: string;
+  outDir?: string;
+  watchAssets?: boolean;
+}
+
+export interface ActionOnFile {
+  action: "change" | "unlink";
+  item: AssetEntry;
+  path: string;
+  sourceRoot: string;
+  watchAssetsMode: boolean;
+}
+
+export interface SwcBuilderOptions {
+  swcrcPath?: string;
+  outDir?: string;
+  filenames?: string[];
+  sync?: boolean;
+  extensions?: string[];
+  copyFiles?: boolean;
+  includeDotfiles?: boolean;
+  quiet?: boolean;
+}
+
+export interface TscBuilderOptions {
+  configPath?: string;
+}
+
+export type BuilderVariant = "tsc" | "swc";
+export type Builder =
+  | BuilderVariant
+  | {
+      type: "swc";
+      options?: SwcBuilderOptions;
+    }
+  | {
+      type: "tsc";
+      options?: TscBuilderOptions;
+    };
+
+export interface CompilerOptions {
+  tsConfigPath?: string;
+  plugins?: string[] | PluginOptions[];
+  assets?: string[];
+  deleteOutDir?: boolean;
+  manualRestart?: boolean;
+  builder?: Builder;
+}
+
+export interface PluginOptions {
+  name: string;
+  options: Record<string, any>[];
+}
+
+export interface GenerateOptions {
+  spec?: boolean | Record<string, boolean>;
+  flat?: boolean;
+  specFileSuffix?: string;
+}
+
+export interface ProjectConfiguration {
+  type?: string;
+  root?: string;
+  entryFile?: string;
+  exec?: string;
+  sourceRoot?: string;
+  compilerOptions?: CompilerOptions;
+}
+
+export interface Configuration {
+  [key: string]: any;
+
+  language?: string;
+  collection?: string;
+  sourceRoot?: string;
+  entryFile?: string;
+  exec?: string;
+  monorepo?: boolean;
+  compilerOptions?: CompilerOptions;
+  generateOptions?: GenerateOptions;
+  projects?: {
+    [key: string]: ProjectConfiguration;
+  };
+}
