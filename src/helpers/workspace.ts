160e4390907ab794c5d710df02c92965c4ee7e04
0x303133 <0x303133@proton.me>
2023-08-03 21:38:08 +0300

    feat: Create Workspace helper

---
+++ src/helpers/workspace.ts
@@ -0,0 +1,12 @@
+import rimraf from "rimraf";
+
+import { getValueOrDefault } from "./getters/value.or.default";
+import { Configuration } from "../configuration";
+
+export class WorkspaceHelper {
+  public async deleteOutDirIfEnabled(configuration: Required<Configuration>, appName: string, dirPath: string) {
+    const isDeleteEnabled = getValueOrDefault<boolean>(configuration, "compilerOptions.deleteOutDir", appName);
+    if (!isDeleteEnabled) return;
+    await rimraf(dirPath);
+  }
+}
